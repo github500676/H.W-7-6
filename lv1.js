3b10a2056794b46bcb35c4cdcf252929cb377633
github500676 <110296780+github500676@users.noreply.github.com>
2022-08-06 00:17:14 +0300

    Hello
    Hello coach this is H.W7+6 you asked for

---
+++ lv1.js
@@ -0,0 +1,15 @@
+var array = [62,10,9,5,0,9,1]
+
+for (var i = 0; i<array.length; i++) {
+    array[i]>10
+}
+
+
+var array = [34,12,7,5,0,6,1]
+
+for (var i = 0; i<array.length; i++) {
+    array[i]/2
+}
+
+
+
